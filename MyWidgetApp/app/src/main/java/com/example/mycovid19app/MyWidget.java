3bcc913f36f4c5e1a3ad679ab72107502ee7c55d
vikadragun <61206707+vikadragun@users.noreply.github.com>
2020-07-14 02:06:40 +0600

    1
    Виджет "Текстовая заметка"

---
+++ MyWidgetApp/app/src/main/java/com/example/mycovid19app/MyWidget.java
@@ -0,0 +1,87 @@
+package com.example.mycovid19app;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import java.util.Arrays;
+
+public class MyWidget extends AppWidgetProvider {
+
+    final static String LOG_TAG = "myLogs";
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+        Log.d(LOG_TAG, "onEnabled");
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        Log.d(LOG_TAG, "onUpdate " + Arrays.toString(appWidgetIds));
+
+        SharedPreferences sp = context.getSharedPreferences(
+                ConfigActivity.WIDGET_PREF, Context.MODE_PRIVATE);
+        for (int id : appWidgetIds) {
+            updateWidget(context, appWidgetManager, sp, id);
+        }
+
+
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+        Log.d(LOG_TAG, "onDeleted " + Arrays.toString(appWidgetIds));
+
+        // Удаляем Preferences
+        SharedPreferences.Editor editor = context.getSharedPreferences(
+                ConfigActivity.WIDGET_PREF, Context.MODE_PRIVATE).edit();
+        for (int widgetID : appWidgetIds) {
+            editor.remove(ConfigActivity.WIDGET_TEXT + widgetID);
+            editor.remove(ConfigActivity.WIDGET_COLOR + widgetID);
+        }
+        editor.commit();
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+        Log.d(LOG_TAG, "onDisabled");
+    }
+
+    static void updateWidget(Context context, AppWidgetManager appWidgetManager,
+                             SharedPreferences sp, int widgetID) {
+        Log.d(LOG_TAG, "updateWidget " + widgetID);
+
+        // Читаем параметры Preferences
+        String widgetText = sp.getString(ConfigActivity.WIDGET_TEXT + widgetID, null);
+        if (widgetText == null) return;
+        int widgetColor = sp.getInt(ConfigActivity.WIDGET_COLOR + widgetID, 0);
+
+        // Настраиваем внешний вид виджета
+        RemoteViews widgetView = new RemoteViews(context.getPackageName(),
+                R.layout.widget);
+        widgetView.setTextViewText(R.id.tv, widgetText);
+        widgetView.setInt(R.id.tv, "setBackgroundColor", widgetColor);
+
+        /////////////////////////////
+        Intent configIntent = new Intent(context, ConfigActivity.class);
+        configIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_CONFIGURE);
+        configIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetID);
+        PendingIntent pIntent = PendingIntent.getActivity(context, widgetID,
+                configIntent, 0);
+        widgetView.setOnClickPendingIntent(R.id.btn_settings, pIntent);
+        ////////////////////////////
+
+        // Обновляем виджет
+        appWidgetManager.updateAppWidget(widgetID, widgetView);
+    }
+}
